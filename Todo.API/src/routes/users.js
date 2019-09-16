5176c95ef7f702b4b90748a0a81d4d4ebdb38b86
Aleksander Grzelak <alek678900@gmail.com>
2019-09-16 22:12:49 +0200

    configure users routes

--- Todo.API/src/routes/users.js
+++ Todo.API/src/routes/users.js
@@ -6,8 +6,13 @@ const usersController = require('../controllers/users');
 // Base route /api/users
 router
     .route('/')
+    .get(usersController.getUsers)
+    .post(usersController.addUser);
+
+router
+    .route('/:id')
     .get(usersController.getUserById)
-
-
+    .put(usersController.updateUser)
+    .delete(usersController.removeUser);
 
 module.exports = router;
