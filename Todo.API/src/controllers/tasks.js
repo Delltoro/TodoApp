7640a11c96e744458d1c8bd5ce7a521abcf2d41f
Aleksander Grzelak <alek678900@gmail.com>
2019-09-17 00:10:57 +0200

    get Task by User

--- Todo.API/src/controllers/tasks.js
+++ Todo.API/src/controllers/tasks.js
@@ -1,4 +1,7 @@
 const { Task, validate } = require('../models/task');
+const {User} = require('../models/user')
+const config = require('config');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
   getTaskById: async function (req, res) {
@@ -14,7 +17,13 @@ module.exports = {
 
   getTasks: async (req, res) => {
         try {
-            const tasks = await Task.find().sort('timeCreated');
+            const token = req.header('x-auth-token');
+            const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+
+            const user = await User.findById(decoded._id);
+            if (!user) return res.status(404).send('A user with the given ID was not found.')
+
+            const tasks = await Task.find({"_id":user.tasks}).sort('timeCreated');
             res.send(tasks);
         } catch (error) {
             res.status(500).send('An error occured.');
