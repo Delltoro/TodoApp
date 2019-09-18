1d3b5087c8e0b476bbbcbeb5ddc26304695dda06
Bohdan <bohdanimiela@gmail.com>
2019-09-18 14:54:31 +0200

    Refactor api endpoints

--- Todo.Client/src/js/RoutesTasks.js
+++ Todo.Client/src/js/RoutesTasks.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import UITasks from './UITasks';
 
+const ENDPOINT = '/api/tasks';
+
 class RoutesTasks {
 
   static async getTasks() {
     try {
-      const tasks = await axios.get(CONFIG.ServerAPI.url + '/api/tasks');
+      const tasks = await axios.get(`${CONFIG.ServerAPI.url}${ENDPOINT}`);
       UITasks.renderTasksList(tasks.data);
     }
     catch(error) {
@@ -15,7 +17,7 @@ class RoutesTasks {
 
   static async getTask(id, option) {
     try {
-      const task = await axios.get(CONFIG.ServerAPI.url + '/api/tasks/' + id);
+      const task = await axios.get(`${CONFIG.ServerAPI.url}${ENDPOINT}/${id}`);
       option === 'expand' ? UITasks.expandTask(task.data) : UITasks.collapseTask(task.data);
     }
     catch(error) {
@@ -25,8 +27,8 @@ class RoutesTasks {
 
   static async setTaskState(id, isDone) {
     try {
-      const task = await axios.get(CONFIG.ServerAPI.url + '/api/tasks/' + id);
-      axios.put(CONFIG.ServerAPI.url + '/api/tasks/' + id, {
+      const task = await axios.get(`${CONFIG.ServerAPI.url}${ENDPOINT}/${id}`);
+      axios.put(`${CONFIG.ServerAPI.url}${ENDPOINT}/${id}`, {
         "isDone": isDone,
         "title": task.data.title,
         "text": task.data.text,
