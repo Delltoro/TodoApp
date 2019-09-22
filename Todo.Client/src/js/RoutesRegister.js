15c420f59da8d8270a8ab15ca256d98a86ce95cb
Celina <celinawierzbicka@gmail.com>
2019-09-22 13:14:36 +0200

    small correction on register files

--- Todo.Client/src/js/RoutesRegister.js
+++ Todo.Client/src/js/RoutesRegister.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const ENDPOINT = '/api/register';
+const ENDPOINT = '/api/users';
 
 class RoutesRegister {
   static async register(event){
@@ -12,9 +12,9 @@ class RoutesRegister {
             username : document.querySelector('#username').value,
             password : document.querySelector('#password').value,
       }
-        const res = await axios.post(`${CONFIG.ServerAPI.url}${ENDPOINT}`, data);
+        const res = await axios.post(`${CONFIG.ServerAPI.url}${ENDPOINT}`, user);
         
-        localStorage["x-auth-token"] = res.data;
+        localStorage["x-auth-token"] = res.user;
 
         window.location.replace(`${window.location.href.split("register.html")[0]}index.html`);
    
